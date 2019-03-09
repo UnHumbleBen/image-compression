febc32118816c7294b7f5a3ebd258da7de0226d1
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-09 15:09:31 -0800

    update main

--- main.m
+++ main.m
@@ -9,8 +9,15 @@ load('ex7data2.mat');
 
 % Select an intial set of centroids
 K = 3; % 3 Centroids
-initial_centroids = [3 3; 6 2; 8 5;]
+initial_centroids = [3 3; 6 2; 8 5;];
 
 % Find the closest centroids for the examples using the
 % initial_centroids
 idx = findClosestCentroids(X, initial_centroids);
+
+fprintf('Closest centroids for the first 3 examples: \n')
+fprintf(' %d', idx(1:3));
+fprintf('\n(the closest centroids should be in 1, 3, 2 respectively\n');
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
