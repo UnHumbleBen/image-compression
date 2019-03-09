d5d93b589ee25e9e631592cf800414d1195aea38
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-09 14:24:41 -0800

    initializing centroids

--- main.m
+++ main.m
@@ -7,3 +7,10 @@ fprintf('Finding closest centroids. \n\n');
 % Load an example dataset  X
 load('ex7data2.mat');
 
+% Select an intial set of centroids
+K = 3; % 3 Centroids
+initial_centroids = [3 3; 6 2; 8 5;]
+
+% Find the closest centroids for the examples using the
+% initial_centroids
+idx = findClosestCentroids(X, initial_centroids);
