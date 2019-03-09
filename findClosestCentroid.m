f0d21d532d15a4ef05ad02b80fee671ac1ca5854
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-09 14:51:42 -0800

    new function for finding closest centroid to a training example

---
+++ findClosestCentroid.m
@@ -0,0 +1,22 @@
+function k = findClosestCentroid(x, centroids)
+%FINDCLOSESTCENTROID computes the centroid membership for an example
+%   k = FINDCLOSESTCENTROID (x, centroids) returns the closest centroid
+%   in centroids to data example x. k is in the range [1..K], where K
+%   is the number of classes.
+
+% Set K
+K = size(centroids, 1);
+k = 0;
+min_distance = Inf;
+
+% Loop over the centroids
+for k_index = 1:K
+    centroid = centroids(k_index);
+    dist = findDistance(x, centroid);
+    if (dist < min_dist)
+        k = k_index;
+        min_distance = dist;
+    endif
+endfor
+
+end
