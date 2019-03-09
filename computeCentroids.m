8c8e9f3cb9238270867f5c9593c41bc03292f068
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-09 15:40:14 -0800

    declared function for computeCentroids

---
+++ computeCentroids.m
@@ -0,0 +1,18 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the
+%data points assigned to each centroid.
+%   centriods = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignment (i.e. each entry in range [1..K])
+%   for each example, and K, the number of centroids. 
+%   returns a matrix centroids, where each row of centroids is the mean of
+%   the data points assigned to it.
+
+% Useful variables
+[m n] = size(X);
+
+% Initialize return value
+centroids = zeros(K, n);
+
+end
