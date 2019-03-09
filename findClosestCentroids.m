0c411982f174d6ca7a8fa9ac1e194490ccb9d3d8
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-09 14:28:33 -0800

    findClosestCentroids.m file added

---
+++ findClosestCentroids.m
@@ -0,0 +1,7 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTOIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+
+end
