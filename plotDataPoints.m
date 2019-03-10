8b60f5d9e2a6443ebe3ecc4413046f3d1e70424c
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-10 01:26:45 -0800

    doc plotDataPoints

--- plotDataPoints.m
+++ plotDataPoints.m
@@ -1,3 +1,8 @@
 function plotDataPoints(X, idx, K)
+%PLOTDATAPOINTS plots data points in X, coloring them so that those with
+%the same index assignments in idx has the same color
+%   PLOTDATAPOINTS(X, idx, K) plots the data points in X, coloring them
+%   so that those with the same index assignments in idx have the same
+%   color
 
 end
