dc42cb60f03fcccdfec83343493611ab7f15237b
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-10 03:01:04 -0700

    plot line

--- drawLine.m
+++ drawLine.m
@@ -3,4 +3,9 @@ function drawLine(p1, p2, varargin)
 %   DRAWLINE(p1, p2) Draws a line from point p1 to point p2 and holds the
 %   current figure
 
+%plot x coordinate matrix [p1(1) p2(1)] versus
+%y coordinate matrix [p1(2) p2(2)]
+%varargin allows for additional parameters
+plot([p1(1) p2(1)], [p1(2) p2(2)], varargin{:});
+
 end
