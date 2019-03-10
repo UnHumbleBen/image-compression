d46ddd3f315055cc47ec1c0642a4890cea66b9a4
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-10 00:03:28 -0800

    testing K Mean clustering

---
+++ testKMeansClustering.m
@@ -0,0 +1,22 @@
+%% Initialization
+clear ; close all; clc
+
+%%K-Means Clustering
+fprintf('\nRunning K-Means clustering on example dataset.\n\n');
+
+%Load an example dataset
+load('exampleData.mat');
+
+% Settings for running K-Means
+K = 3;
+max_iters = 10;
+
+% For consistency, initializing centroids to specific values
+initial_centroids = [3 3; 6 2; 8 5];
+
+% Run K-means algorithm
+[centroids, idk] = runMeans(X, initial_centroids, max_iters, true);
+fprintf('\nK-Means Done.\n\n');
+
+fprintf('Program paused. Press enter to continue.\n');
+pause; 
