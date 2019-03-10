39cd5dc4a7f64a223a898e3523caff0b3d1bf3a7
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-10 01:19:13 -0800

    outputing results and assigning centriods

--- runkMeans.m
+++ runkMeans.m
@@ -31,4 +31,26 @@ centroids = initial_centroids;
 previous_centroids = centriods;
 idx = zeros(m, 1);
 
+% Run K-Means
+for i=1:max_iters
+
+    % Output progress
+    fprintf('K-Means iteration %d/%d...\n', i, max_iters);
+    if exist('OCTAVE_VERSION')
+        fflush(stdout);
+    end
+
+    % For each example in X, assign it to the closest centroid
+    idx = findClosestCentroids(X, centroids);
+
+    % Optionally, plot progress here
+    if plot_progress
+        plotProgresskMeans(X, centroids, previous_centroids, idx, K, i);
+        previous_centroids = centriods;
+        fprintf('Press enter to continue.\n');
+        pause;
+    end
+
+end
+
 end
