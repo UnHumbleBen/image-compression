260ee90d96d92285ceabec209ef9dbac31bc8444
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-10 10:42:17 -0700

    initialize return value

--- kMeansInitCentroids.m
+++ kMeansInitCentroids.m
@@ -4,4 +4,7 @@ function centroids = kMeansInitCentroids(X, K)
 %   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids
 %   to be used with the K-Means on the dataset X
 
+%Initialize the values
+centroids = zeros(K, size(X, 2)); %size(X, 2) is n, the number of features
+
 end
