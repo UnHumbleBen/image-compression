ada0332097ea0e3b8552294b4255f45a8bd80dcb
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-10 01:40:10 -0800

    plot the data

--- plotDataPoints.m
+++ plotDataPoints.m
@@ -9,4 +9,12 @@ function plotDataPoints(X, idx, K)
 palette = hsv(K + 1);     % creates a color map
 colors = palette(idx, :); % color [m x 3] is color assignments to data
 
+% Plot the data
+scatter(X(:,1), X(:,2), 15, colors); %plots x1 = X(:,1) on the x axis
+                                     %plots x2 = X(:,2) on the y axis
+                                     %15 is size of the market
+                                     %colors is m x 3 matrix defining
+                                     %the RGB color of each market
+                                     %individually
+
 end
