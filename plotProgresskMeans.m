2eaed13abcb4d61f3e96a6609ad4bc487babc4ac
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-10 01:22:49 -0800

    documented plotProgresskMeans

--- plotProgresskMeans.m
+++ plotProgresskMeans.m
@@ -1,3 +1,12 @@
 function plotProgresskMeans(X, centroids, previous, idk, K, i)
+%PLOTPROGRESSKMEANS is a helper function that dipslays the progress of
+%k-MEans as it is running. It is intended for use only with 2D data.
+%   PLOTPROGRESSKMEANS(X, centroids, previous, idx, K, i) plots the data
+%   points with colors assigned to each centroid. With the previous
+%   centroids, it also plots a line between the previous locations and
+%   current locations of the centroids.
+%
+
+
 
 end
