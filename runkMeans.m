1c1e47cbbff85eccee112b59ea6e092e90f33c78
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-10 01:12:36 -0800

    initialization of values

--- runkMeans.m
+++ runkMeans.m
@@ -13,4 +13,22 @@ function [centroids, idx] = runkMeans(X, initial_centroids, ...
 %   centroids, a K x n matrix of the computed centroids and idx, a m x 1
 %   vector of the centroid assignments (i.e. each entry in range [1..K])
 
+% Set default value for plot progress
+if ~exist('plot_progress', 'var') || isempty(plot_progress)
+    plot_progress = false;
+end
+
+% Plot the data if we are plotting progress
+if plot_progress
+    figure;
+    hold on;
+end
+
+%Initialize values
+[m n] = size(X);
+K = size(initial_centroids, 1);
+centroids = initial_centroids;
+previous_centroids = centriods;
+idx = zeros(m, 1);
+
 end
