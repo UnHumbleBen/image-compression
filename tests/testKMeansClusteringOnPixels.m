ffe3929ab4d00b12c8c6a4fa6c9f26d68fe81399
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-10 21:04:00 -0700

    normalize pixel values

--- tests/testKMeansClusteringOnPixels.m
+++ tests/testKMeansClusteringOnPixels.m
@@ -11,3 +11,6 @@ fprintf('\nRunning K-Means clustering on pixels from an image.\n\n');
 %       and the last index represents red, green, or blue
 %   double converts the numbers to double precision type
 A = double(imread('../images/bird_small.png'));
+
+% Divide by 255 so that all the values are in the range 0 - 1
+A = A / 255;
