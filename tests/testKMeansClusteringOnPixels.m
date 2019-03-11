88a76f1c632c61997b90819af63f9ce98398fe7b
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-10 20:58:30 -0700

    loaded image

--- tests/testKMeansClusteringOnPixels.m
+++ tests/testKMeansClusteringOnPixels.m
@@ -3,3 +3,11 @@ clear ; close all; clc
 addpath('../src');
 
 fprintf('\nRunning K-Means clustering on pixels from an image.\n\n');
+
+%   Load an image of a bird
+%   imread loads the image as a h x w x 3 matrix
+%       where h is the image height (in pixels)
+%       where w is the image width  (in pixels)
+%       and the last index represents red, green, or blue
+%   double converts the numbers to double precision type
+A = double(imread('../images/bird_small.png'));
